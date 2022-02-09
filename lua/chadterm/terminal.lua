2706260c0d28ba55b002b765894de3ffb8683de5
zbirenbaum <zacharyobirenbaum@gmail.com>
2022-02-08 21:42:48 -0500

    "cleanup"

--- lua/chadterm/terminal.lua
+++ lua/chadterm/terminal.lua
@@ -20,47 +20,48 @@ local function on_action(chadterms)
    Globals.chadterms = chadterms
 end
 
+local function on_new_buf()
+   local bufs = vim.api.nvim_list_bufs()
+   local term_buf_id = bufs[#bufs]
+   vim.api.nvim_input('i') --term enter
+   return term_buf_id
+end
+
+local function on_new_win()
+   local wins = vim.api.nvim_list_wins()
+   local term_win_id = wins[#wins]
+   vim.api.nvim_set_current_win(term_win_id)
+   return term_win_id
+end
+local function spawn(spawn_cmd, type)
+   vim.cmd(spawn_cmd)
+   return type == "win" and on_new_win() or type == "buf" and on_new_buf()
+end
+
+
+
 function M.new_or_toggle (direction, dims)
    local chadterms = Globals.chadterms
-   print(vim.inspect(dims))
    local cmds = get_cmds(dims)
 
    local function new_term()
-      vim.cmd(cmds[direction]["new"])
-      local wins = vim.api.nvim_list_wins()
-      local term_win_id = wins[#wins]
-      vim.api.nvim_set_current_win(term_win_id)
-      vim.cmd("term")
-      vim.api.nvim_input('i')
-      local bufs = vim.api.nvim_list_bufs()
-      local term_buf_id = bufs[#bufs]
+      local term_win_id=spawn(cmds[direction]["new"], "win")
+      local term_buf_id=spawn("term", "buf")
       chadterms[direction][1] = { win = term_win_id, buf = term_buf_id }
    end
 
    local function hide_term()
       local term_id = chadterms[direction][1]["win"]
-      if term_id then
-         vim.api.nvim_set_current_win(term_id)
-         vim.cmd('hide')
-      end
+      vim.api.nvim_set_current_win(term_id)
+      vim.cmd('hide')
+      --no update necessary, win will be invalid on hide
    end
 
    local function show_term()
-      print("show")
       local term_buf_id = chadterms[direction][1]["buf"]
-      if vim.api.nvim_buf_is_valid(term_buf_id) then
-         print(term_buf_id)
-         vim.cmd(cmds[direction]["new"])
-         local wins = vim.api.nvim_list_wins()
-         local term_win_id = wins[#wins]
-         vim.api.nvim_set_current_win(term_win_id)
-         vim.api.nvim_set_current_buf(term_buf_id)
-         chadterms[direction][1]["win"] = term_win_id
-         chadterms[direction][1]["buf"] = term_buf_id
-         vim.api.nvim_input('i')
-      else
-         new_term()
-      end
+      local term_win_id = spawn(cmds[direction]["new"], "win")
+      vim.api.nvim_set_current_buf(term_buf_id)
+      chadterms[direction][1] = { win = term_win_id, buf = term_buf_id }
    end
 
    local opened = chadterms[direction]
@@ -71,7 +72,6 @@ function M.new_or_toggle (direction, dims)
    elseif vim.api.nvim_buf_is_valid(chadterms[direction][1]["buf"]) then
       show_term()
    else
-      print("new")
       new_term()
    end
    on_action(chadterms)
