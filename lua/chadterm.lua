267a08be3d90bb38ffe2972d409110f3e9b12da7
zbirenbaum <zacharyobirenbaum@gmail.com>
2022-02-08 21:55:36 -0500

    "removed globals.lua"

--- lua/chadterm.lua
+++ lua/chadterm.lua
@@ -1,5 +1,14 @@
 local M = {}
 
+local init_globals = function()
+   Globals = {
+      chadterms = {
+         horizontal = {},
+         vertical = {}
+      },
+   }
+end
+
 M.setup = function (user_config)
    local config = {
       keymaps = {
@@ -18,7 +27,7 @@ M.setup = function (user_config)
    if user_config ~= nil and not vim.tbl_isempty(user_config) then
       config = vim.tbl_deep_extend(config, user_config)
    end
-   require("chadterm.globals").init_globals()
+   init_globals()
    require("chadterm.utils").config_handler(config)
 end
 
