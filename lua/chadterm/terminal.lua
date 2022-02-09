9fbdc80ea2d8c5e7fab72d2b4fd0ff9c9e86a734
zbirenbaum <zacharyobirenbaum@gmail.com>
2022-02-08 21:01:50 -0500

    "debug"

--- lua/chadterm/terminal.lua
+++ lua/chadterm/terminal.lua
@@ -51,10 +51,10 @@ function M.new_or_toggle (direction, dims)
       if vim.api.nvim_buf_is_valid(term_buf_id) then
          print(term_buf_id)
          vim.cmd("unhide " .. term_buf_id)
-      --    vim.cmd(cmds[direction]["new"])
-      --    local wins = vim.api.nvim_list_wins()
-      --    vim.api.nvim_set_current_win(wins[#wins])
-      --    vim.api.nvim_win_set_buf(0, term_buf)
+         vim.cmd(cmds[direction]["new"])
+         local wins = vim.api.nvim_list_wins()
+          vim.api.nvim_set_current_win(wins[#wins])
+          vim.api.nvim_win_set_buf(0, term_buf_id)
       --    chadterms[direction][1]["win"] = wins[#wins]
       else
          new_term()
